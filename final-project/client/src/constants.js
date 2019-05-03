6cb1a9f2c0012462dd43d9bab20a41ab3169d607
Sean Casey <sean@seanjcasey.com>
2019-05-03 15:03:04 -0400

    added constants with time and token address hash tables

---
+++ final-project/client/src/constants.js
@@ -0,0 +1,20 @@
+export const TOKENTABLE = {
+    '0xBd4Ac9375F7cfA025b9BA06A202c7abF24973c40': {
+        name: 'Sean Token',
+    },
+    '0x728B86683950F559E3F64c4F57ff0776906Fd0Ff': {
+        name: 'Pavan Token',
+    },
+    '0x19342108sdaf9019E3F64c4F57ff0776906Fd0Ff': {
+        name: 'Consensys Token',
+    },
+}
+
+export const TIMETABLE = {
+    3600: '1 hour',
+    86400: '1 day',
+    604800: '1 week',
+    18144000: '1 month',
+}
+
+
