bb6142b1e101c2caef976cdd9539bd15cb63d37d
Sean Casey <sean@seanjcasey.com>
2019-05-06 10:20:37 -0400

    format contract object and use new date formatter for easy processing in orders table

--- final-project/client/src/components/OrderTable.js
+++ final-project/client/src/components/OrderTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { TOKENTABLE, TIMETABLE } from '../constants';
+import { dateObjDisplayFormatter } from '../utils';
 
 class OrderTable extends Component {
   constructor(props) {
@@ -45,17 +46,20 @@ class OrderTable extends Component {
 
     let orders = [];
     for (const orderKey of orderKeys) {
-      const order = drizzleState.contracts.CostAverageOrderBook.getOrderForOwnerIndex[orderKey];
-      if (order) {
-        orders.push(
-          {
-            'id': order.value.id_,
-            'amount': drizzle.web3.utils.fromWei(order.value.amount_, 'ether'),
-            'targetCurrency': order.value.targetCurrency_,
-            'frequency': order.value.frequency_,
-            'batches': order.value.batches_
-          }
-        );
+      const orderState = drizzleState.contracts.CostAverageOrderBook.getOrderForOwnerIndex[orderKey];
+      if (orderState) {
+        const order = {
+          'id': orderState.value.id_,
+          'amount': drizzle.web3.utils.fromWei(orderState.value.amount_, 'ether'),
+          'targetCurrency': orderState.value.targetCurrency_,
+          'frequency': Number(orderState.value.frequency_),
+          'batches': Number(orderState.value.batches_),
+          'batchesExecuted': Number(orderState.value.batchesExecuted_),
+          'targetCurrencyConverted': Number(orderState.value.targetCurrencyConverted_),
+          'lastConversionTimestamp': Number(orderState.value.lastConversionTimestamp_)
+        }
+        order['nextConversionTimestamp'] = order['lastConversionTimestamp'] > 0 ? order['lastConversionTimestamp'] + order['frequency'] : 0;
+        orders.push(order);
       }
     }
 
@@ -71,6 +75,9 @@ class OrderTable extends Component {
               <th scope="col">Target Token</th>
               <th scope="col">Frequency</th>
               <th scope="col">Batches</th>
+              <th scope="col">Converted</th>
+              <th scope="col">Last</th>
+              <th scope="col">Next</th>
             </tr>
           </thead>
           <tbody>
@@ -81,7 +88,10 @@ class OrderTable extends Component {
                   <td>{order.amount}</td>
                   <td>{TOKENTABLE[order.targetCurrency] ? TOKENTABLE[order.targetCurrency].name : order.targetCurrency}</td>
                   <td>{TIMETABLE[order.frequency] ? TIMETABLE[order.frequency] : `${order.frequency} seconds `}</td>
-                  <td>{order.batches}</td>
+                  <td>{order.batchesExecuted} / {order.batches}</td>
+                  <td>{order.targetCurrencyConverted} {TOKENTABLE[order.targetCurrency] ? TOKENTABLE[order.targetCurrency].symbol : ""}</td>
+                  <td>{order.lastConversionTimestamp > 0 ? dateObjDisplayFormatter(new Date(order.lastConversionTimestamp * 1000)) : "n/a"}</td>
+                  <td>{order.nextConversionTimestamp > 0 ? dateObjDisplayFormatter(new Date(order.nextConversionTimestamp * 1000)) : "overdue!"}</td>
                 </tr>
               );
             })}
