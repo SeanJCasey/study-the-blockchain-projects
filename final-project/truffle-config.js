8207f0ac558827b6a2b4f7c4262307d9ff01d8b3
Sean Casey <sean@seanjcasey.com>
2019-05-23 16:46:37 -0400

    configured truffle-config for rinkeby

--- final-project/truffle-config.js
+++ final-project/truffle-config.js
@@ -18,16 +18,15 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-const path = require("path");
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = '9602c950c9374036ad1ced3b456d204f';
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
+const path = require('path');
 
 module.exports = {
   // Need to do this to get contracts inside the create-react-app folder
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -63,14 +62,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    },
 
     // Useful for private networks
     // private: {
